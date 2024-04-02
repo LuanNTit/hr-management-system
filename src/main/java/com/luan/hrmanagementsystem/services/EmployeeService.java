e27e77f3c1152f3fd27f1ac0d578868fc79ed31a
Nguyễn Trần Luân <nguyentranluanit@gmail.com>
2024-04-02 17:13:27 +0700

    refactory better code

--- src/main/java/com/luan/hrmanagementsystem/services/EmployeeService.java
+++ src/main/java/com/luan/hrmanagementsystem/services/EmployeeService.java
@@ -1,116 +1,19 @@
 package com.luan.hrmanagementsystem.services;
 
 import java.util.List;
-import java.util.Optional;
-
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
 
 import com.luan.hrmanagementsystem.models.Employee;
-import com.luan.hrmanagementsystem.repositories.EmployeeRepository;
 
-import lombok.extern.slf4j.Slf4j;
-@Service
-@Slf4j
-public class EmployeeService implements IEmployeeService {
+public interface IEmployeeService {
+	public Employee createEmployee(Employee employee);
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+	public Employee updateEmployee(Long id, Employee employee);
 
-    @Override
-    public Employee createEmployee(Employee employee) {
-        try {
-            // Kiểm tra hợp lệ trước khi lưu vào cơ sở dữ liệu
-            validateEmployee(employee);
+	public void deleteEmployee(Long id);
 
-            Employee saveEmployee = employeeRepository.save(employee);
-            log.info("Employee {} is saved", saveEmployee.getId());
-            return saveEmployee;
-        } catch (DataIntegrityViolationException | ConstraintViolationException e) {
-            // Xử lý ngoại lệ khi vi phạm ràng buộc cơ sở dữ liệu
-            throw new IllegalArgumentException("Duplicate employee details or data integrity violation");
-        } catch (DataAccessException e) {
-            // Xử lý ngoại lệ cơ sở dữ liệu khác
-            throw new RuntimeException("Error while accessing the database", e);
-        }
-    }
+	public Employee getEmployeeById(Long id);
 
+	public List<Employee> getAllEmployees();
 
-    @Override
-    public Employee updateEmployee(Long id, Employee employee) {
-        // Kiểm tra xem nhân viên có tồn tại không
-        Optional<Employee> existingEmployeeOptional = employeeRepository.findById(id);
-        if (existingEmployeeOptional.isPresent()) {
-            Employee existingEmployee = existingEmployeeOptional.get();
-            
-            // kiểm tra thông tin có bị trùng lặp không
-            validateEmployee(employee);
-            
-            // Cập nhật thông tin nhân viên
-            existingEmployee.setName(employee.getName());
-            existingEmployee.setDateOfBirth(employee.getDateOfBirth());
-            existingEmployee.setPosition(employee.getPosition());
-            existingEmployee.setSalary(employee.getSalary());
-            existingEmployee.setAge(employee.getAge());
-            // Cập nhật các trường khác tùy theo yêu cầu
-
-            // Lưu vào cơ sở dữ liệu
-            employeeRepository.save(existingEmployee);
-            return existingEmployee;
-        } else {
-            throw new IllegalArgumentException("Employee not found with ID: " + id);
-        }
-    }
-
-    @Override
-    public void deleteEmployee(Long id) {
-        // Kiểm tra xem nhân viên có tồn tại không
-        Optional<Employee> existingEmployeeOptional = employeeRepository.findById(id);
-        if (existingEmployeeOptional.isPresent()) {
-            // Xóa nhân viên nếu tồn tại
-            employeeRepository.deleteById(id);
-        } else {
-            throw new IllegalArgumentException("Employee not found with ID: " + id);
-        }
-    }
-
-    @Override
-    public Employee getEmployeeById(Long id) {
-    	// Kiểm tra xem nhân viên có tồn tại không
-        Optional<Employee> existingEmployeeOptional = employeeRepository.findById(id);
-        if (existingEmployeeOptional.isPresent()) {
-        	return existingEmployeeOptional.orElse(null);
-        } else {
-            throw new IllegalArgumentException("Employee not found with ID: " + id);
-        }
-    }
-
-    @Override
-    public List<Employee> getAllEmployees() {
-        try {
-            // Lấy danh sách tất cả nhân viên
-            return employeeRepository.findAll();
-        } catch (DataAccessException e) {
-            // Xử lý ngoại lệ chung của truy xuất cơ sở dữ liệu
-            throw new RuntimeException("Error while accessing the database", e);
-        }
-    }
-
-    private void validateEmployee(Employee employee) {
-        // Tìm tất cả nhân viên có cùng các thuộc tính như employee
-        List<Employee> foundEmployees = employeeRepository.findByName(employee.getName());
-
-     // Kiểm tra tính đồng đạc giữa các nhân viên đã tìm được và employee
-        if (foundEmployees.stream().anyMatch(existingEmployee ->
-                existingEmployee.getName().equals(employee.getName()) &&
-        		existingEmployee.getDateOfBirth().getTime() == employee.getDateOfBirth().getTime()
-                )) {
-            throw new IllegalArgumentException("Employee with the same details already exists");
-        }
-
-    }
-
+	// Các phương thức khác liên quan đến quản lý nhân sự
 }
--- src/main/java/com/luan/hrmanagementsystem/services/UserService.java
+++ src/main/java/com/luan/hrmanagementsystem/services/UserService.java
@@ -1,124 +1,14 @@
 package com.luan.hrmanagementsystem.services;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.stereotype.Service;
-
-import com.luan.hrmanagementsystem.database.Database;
 import com.luan.hrmanagementsystem.models.User;
-import com.luan.hrmanagementsystem.repositories.UserRepository;
-
-@Service
-public class UserService implements IUserService,  UserDetailsService {
-	private static final Logger logger = LoggerFactory.getLogger(Database.class);
-	@Autowired
-	private UserRepository userRepository;
-
-	@Override
-	public List<User> getAllUsers() {
-		try {
-			List<User> users = userRepository.findAll();
-			logger.info("Number of users fetched: {}", users.size());
-			return users;
-		} catch (Exception e) {
-			throw new RuntimeException("Error while fetching users", e);
-		}
-	}
-
-	@Override
-	public User getUserById(Long userId) {
-		// Kiểm tra xem nhân viên có tồn tại không
-		Optional<User> existingUserOptional = userRepository.findById(userId);
-		if (existingUserOptional.isPresent()) {
-			return existingUserOptional.orElse(null);
-		} else {
-			throw new IllegalArgumentException("User not found with ID: " + userId);
-		}
-	}
-
-	@Override
-	public User createUser(User user) {
-		// Bổ sung logic kiểm tra trước khi lưu người dùng
-		Optional<User> existingUser = userRepository.findByUserName(user.getUserName());
-		if (existingUser.isPresent()) {
-			// Xử lý trường hợp trùng tên người dùng (hoặc bất kỳ điều kiện nào khác)
-			throw new IllegalArgumentException("Username already exists: " + user.getUserName());
-		}
-
-		/*
-		 * user.setEncryptedPassword(passwordEncoder.encode(user.getEncryptedPassword())
-		 * );
-		 */
-
-		// Lưu người dùng mới vào cơ sở dữ liệu
-		User savedUser = userRepository.save(user);
-		return savedUser;
-	}
-
-	@Override
-	public User updateUser(Long userId, User updatedUser) {
-		Optional<User> existingUserOptional = userRepository.findById(userId);
-
-		if (existingUserOptional.isPresent()) {
-			User existingUser = existingUserOptional.get();
-			existingUser.setEnabled(updatedUser.isEnabled());
-			// Mã hóa mật khẩu trước khi lưu vào cơ sở dữ liệu
-			String encodedPassword = BCrypt.hashpw(existingUser.getEncryptedPassword(), BCrypt.gensalt());
-			existingUser.setEncryptedPassword(encodedPassword);
-			existingUser.setUserName(updatedUser.getUserName());
-
-			return userRepository.save(existingUser);
-		} else {
-			throw new IllegalArgumentException("User not found with id: " + userId);
-		}
-	}
-
-	@Override
-	public void deleteUser(Long userId) {
-		userRepository.deleteById(userId);
-	}
-
-	@Override
-	public List<User> getAllUsers(int page, int size) {
-		Pageable pageable = PageRequest.of(page, size);
-		return userRepository.findAll(pageable).getContent();
-	}
-
-	@Override
-	public List<User> getAllUsers(String sortBy) {
-		Sort sort = Sort.by(sortBy);
-		return userRepository.findAll(sort);
-	}
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<User> user = userRepository.findByUserName(username);
-		if (user.isPresent()) {
-			var userObj = user.get();
-			return org.springframework.security.core.userdetails.User.builder()
-					.username(userObj.getUserName())
-					.password(userObj.getEncryptedPassword())
-					.roles(getRoles(userObj))
-					.build();
-		} else {
-			throw new UsernameNotFoundException(username);
-		}
-	}
-	private String[] getRoles(User user){
-		if (user.getRole() == null) {
-			return new String[] {"USER"};
-		}
-		return user.getRole().split(",");
-	}
+public interface IUserService {
+    List<User> getAllUsers();
+    List<User> getAllUsers(int page, int size);
+    List<User> getAllUsers(String sortBy);
+    User createUser(User user);
+    User updateUser(Long userId, User updatedUser);
+    void deleteUser(Long userId);
+    User getUserById(Long userId);
 }
