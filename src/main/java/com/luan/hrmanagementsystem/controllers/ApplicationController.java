e8c924b72d6c539e3a28ba84995aa8de1be62389
Nguyễn Trần Luân <nguyentranluanit@gmail.com>
2024-04-03 21:11:47 +0700

    feat(ui-admin): update ui admin template

--- src/main/java/com/luan/hrmanagementsystem/controllers/ApplicationController.java
+++ src/main/java/com/luan/hrmanagementsystem/controllers/ApplicationController.java
@@ -10,9 +10,34 @@ public class ApplicationController {
 		return "index";
 	}
 	
-	@GetMapping("/widgets")
+	@GetMapping("/hr")
 	public String widget() {
-		return "widgets";
+		return "hr/index";
+	}
+
+	@GetMapping("/fleet")
+	public String fleet() {
+		return "fleet/index";
+	}
+
+	@GetMapping("/helpdesk")
+	public String helpdesk() {
+		return "helpdesk/index";
+	}
+
+	@GetMapping("/accounts")
+	public String accounts() {
+		return "accounts/index";
+	}
+
+	@GetMapping("/payroll")
+	public String payroll() {
+		return "payroll/index";
+	}
+
+	@GetMapping("/parameters")
+	public String parameters() {
+		return "parameters/index";
 	}
 }
 
